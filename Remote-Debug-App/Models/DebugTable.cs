768a2b1671691e46bd28a5a7328880f6ee35545a
commit <animatrikul@elive.ru>
2020-11-13 09:19:58 +0300

    first commit

---
+++ Remote-Debug-App/Models/DebugTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Remote_Debug_App.Models
+{
+    public partial class DebugTable
+    {
+        public int Id { get; set; }
+        public string RandomGuid { get; set; }
+        public DateTime DateTime { get; set; }
+        public string SerializedObject { get; set; }
+    }
+}
